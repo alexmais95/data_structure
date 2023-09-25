cc9b3c97b660c30ecb25926c78a4dd96ec6f45bd
alexmais <alexlove9596@gmail.com>
2023-09-25 15:46:42 +0300

    create dell methods

--- bin_tree/binary_tree.py
+++ bin_tree/binary_tree.py
@@ -1,56 +1,81 @@
 class Box:
     def __init__(self, data):
         self.data = data
-        self.right = None
         self.left = None
+        self.right = None
 
 
 class Tree:
     def __init__(self):
         self.root = None
 
-    def __find(self, box, parent_box, item):
+    def __find_item(self, parent_box, box, item):
         if box is None:
-            return None, parent_box, False
+            return parent_box, None, False
+
         if item == box.data:
-            return box, parent_box, True
+            return parent_box, box, True
+
         if item < box.data:
             if box.left:
-                return self.__find(box.left, box, item)
+                return self.__find_item(box, box.left, item)
         if item > box.data:
             if box.right:
-                return self.__find(box.right, box, item)
-        return box, parent_box, False
+                return self.__find_item(box, box.right, item)
+        return parent_box, box, False
 
     def append(self, item):
         if self.root is None:
             self.root = item
             return item
 
-        box, parent_box, fl_find = self.__find(self.root, None, item.data)
+        parent_box, box, flag_find = self.__find_item(None, self.root, item.data)
 
-        if not fl_find and box:
+        if not flag_find and box:
             if item.data < box.data:
                 box.left = item
             if item.data > box.data:
                 box.right = item
 
-        return item
-
-    def show_tree(self, node):
-        if node is None:
+    def show_tree(self, root):
+        if root is None:
             return
 
-        self.show_tree(node.left)
-        print(node.data)
-        self.show_tree(node.right)
+        self.show_tree(root.left)
+        print(root.data)
+        self.show_tree((root.right))
+
+    def dell__lif(self, parent_box, box):
+        if parent_box.left == box:
+            parent_box.left = None
+        if parent_box.righr == box:
+            parent_box.righr = None
 
 
-item = [10, 5, 7, 16, 13, 2, 20]
+    def dell_one_child(self, parent_box, box):
+        if parent_box.left == box:
+            if box.left is None:
+                parent_box.left = box.right
+            elif box.right is None:
+                parent_box.left = box.left
+
+        if parent_box.right == box:
+            if box.right is None:
+                parent_box.right = box.left
+            if box.left is None:
+                parent_box.right = box.right
+    def dell__(self, item):
+        parent_box, box, flag_find = self.__find_item(None, self.root, item)
+
+        if not flag_find:
+            return None
+        if box.left is None and box.right is None:
+            self.dell__lif(parent_box, box)
+        elif box.left is None or box.right is None:
+            self.dell_one_child(parent_box, box)
+
 
 tree = Tree()
-
-for x in item:
-    tree.append(Box(x))
-
+tree.append(Box(1))
+tree.append(Box(2))
 tree.show_tree(tree.root)
